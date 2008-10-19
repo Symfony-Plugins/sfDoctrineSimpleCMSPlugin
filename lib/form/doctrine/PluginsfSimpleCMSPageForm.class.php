06fb9c9f768bb32943773a57a4d3f951e9df58b4
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-18 23:16:39 -0500

    [1.2] sfDoctrineSimpleCMSPlugin: Initial entry of forms
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineSimpleCMSPlugin/trunk@12246 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/form/doctrine/PluginsfSimpleCMSPageForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginsfSimpleCMSPage form.
+ *
+ * @package    form
+ * @subpackage sfSimpleCMSPage
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginsfSimpleCMSPageForm extends BasesfSimpleCMSPageForm
+{
+}
---
+++ lib/form/doctrine/PluginsfSimpleCMSSlotForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginsfSimpleCMSSlot form.
+ *
+ * @package    form
+ * @subpackage sfSimpleCMSSlot
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginsfSimpleCMSSlotForm extends BasesfSimpleCMSSlotForm
+{
+}
